a4f0e5e0561e03a35ab5e50365bca3eb754058ad
pultyp <Milyutin1104@gmail.com>
2026-01-31 17:06:03 +0300

    Лабораторная 1.

---
+++ Базовые типы языка Python и работа с ними/Лабораторные работы/Замена пропущенного элемента средним арифметическим/task1_lab1.py
@@ -0,0 +1,18 @@
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+# TODO заменить значение пропущенного элемента средним арифметическим
+
+none_index = numbers.index(None)
+
+sum_numbers = 0
+for num in numbers:
+    if num is not None:
+        sum_numbers += num
+
+count_elements = len(numbers)
+
+average = sum_numbers / count_elements
+
+numbers[none_index] = average
+
+print("Измененный список:", numbers)
---
+++ Базовые типы языка Python и работа с ними/Лабораторные работы/Расчет количества книг на дискете/task2_lab1.py
@@ -0,0 +1,13 @@
+# TODO Найдите количество книг, которое можно разместить на дискете
+
+size_in_mb = 1.44
+pages_in_boook = 100
+rows_on_page = 50
+chars_in_row = 25
+size_of_char_in_bytes = 4
+
+size_of_one_book_in_mb = (size_of_char_in_bytes * chars_in_row * rows_on_page * pages_in_boook)/(1024*1024)
+
+total_books = int(size_in_mb // size_of_one_book_in_mb)
+
+print("Количество книг, помещающихся на дискету:", total_books)
